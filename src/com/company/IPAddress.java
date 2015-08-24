3958d3a5fa07d9971d068441207685bdf63ff4b6
shreeja <shreeja@plank.life>
2015-08-24 15:21:37 +0530

    IPAdress my first commit

---
+++ src/com/company/IPAddress.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.net.InetAddress;
+
+public class IPAddress {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(InetAddress.getLocalHost());
+
+    }
+}
